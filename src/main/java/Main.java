198075bc3ad13653c520f5435d973fc4c034e721
Antonio <oz0969991415@gmail.com>
2023-05-11 15:12:57 +0300

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import DataBase.DataBase;
+import DbServises.DatabaseQueryService;
+
+import java.sql.SQLException;
+
+public class Main {
+
+
+    public static void main(String[] args) throws SQLException {
+        DataBase db = DataBase.getInstance();
+        new DatabaseQueryService().findProjectPrice(db);
+    }
+}
